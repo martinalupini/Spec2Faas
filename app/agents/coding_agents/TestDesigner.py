36b5e712115e0d982c860cf0859b9b78b20d510d
martinalupini <martinalupini11@gmail.com>
2025-11-07 21:04:47 +0100

    Other results for the debugger. First implementattion of Designer_evaluator.py

--- app/agents/coding_agents/TestDesigner.py
+++ app/agents/coding_agents/TestDesigner.py
@@ -2,31 +2,12 @@ from autogen_core import MessageContext, RoutedAgent, message_handler, AgentId
 from autogen_core.models import ChatCompletionClient, SystemMessage, UserMessage
 from .messages.MessagesTypes import *
 from .utils.Utils import *
-import ollama
+from experiments.MessageTypesTest import *
+import time
 
 class TestDesigner(RoutedAgent):
     def __init__(self, llm: str, model_client: ChatCompletionClient) -> None:
         super().__init__("Skilled test designer")
-        self._system_prompt = """You are a very skilled test designer specializing in designing comprehensive and well-documented test cases for Python functions.
-                    Your goal is to generate exhaustive test cases for the given function specification, covering all relevant input scenarios and ensuring correctness, robustness, and edge coverage.
-                    <INSTRUCTIONS>
-                    First of all, make CORRECT tests.
-                    - If the specification is not complete make REASONABLE assumptions on how the function should behave.
-                    - Focus on correctness, clarity, and diversity of test inputs.
-                    - Each test case must:
-                    -- Follow the exact format below.
-                    -- Include a short, meaningful comment or description of what it tests.
-                    -- Be self-contained and syntactically valid Python.
-                    -- Be semantically correct with respect to the specification.
-                    - Only output test cases in the specified format Python format, nothing else.
-                    - After you have produced the tests take your time to reflect if each on of them is correct.
-                    </INSTRUCTIONS>
-                    <OUTPUT FORMAT>
-                    The format of test cases should be:
-                    ```python
-                    assert function_name(input) == expected_output, 'Test Case <number>: Description'
-                    ```
-                    </OUTPUT FORMAT>\n"""
         self._system_messages = [SystemMessage(
             content="You are a very skilled test designer specializing in designing comprehensive and well-documented test cases for Python functions."
                     "Your goal is to generate exhaustive test cases for the given function specification, covering all relevant input scenarios and ensuring correctness, robustness, and edge coverage."
@@ -57,47 +38,36 @@ class TestDesigner(RoutedAgent):
     async def handle_generate_code_message(self, message: CodeMessage, ctx: MessageContext) -> Message:
         print_green(f"{self.id.type} received message. Creating now a test suite for the function.")
 
-        if self._llm == "gemini-2.5-pro" or self._llm == "gemini-2.0-flash":
-            # Prepare input to the chat completion model.
-            prompt = "Function specification: " + message.specification + "\nFunction signature: " + message.function_signature + "\nPlease generate the tests:"
-            user_message = UserMessage(content=prompt, source="user")
-            response = await self._model_client.create(
-                self._system_messages + [user_message], cancellation_token=ctx.cancellation_token
-            )
+        # Prepare input to the chat completion model.
+        prompt = "Function specification: " + message.specification + "\nFunction signature: " + message.function_signature + "\nPlease generate the tests:"
+        user_message = UserMessage(content=prompt, source="user")
+        response = await self._model_client.create(
+            self._system_messages + [user_message], cancellation_token=ctx.cancellation_token
+        )
 
-            assert isinstance(response.content, str)
-            dialogue(response.content, self._role)
-            return_message = await self._runtime.send_message(
-                CodeMessage(message.specification, message.function_signature, "", response.content, self.id.type),
-                AgentId("test_executor", "default"))
-            return return_message
-        else:
+        assert isinstance(response.content, str)
+        dialogue(response.content, self._role)
+        return_message = await self._runtime.send_message(
+            CodeMessage(message.specification, message.function_signature, "", response.content, self.id.type),
+            AgentId("test_executor", "default"))
+        return return_message
 
-            if self._client is None:
-                self._client = ollama.Client(host='http://160.80.97.151:11434')
+    @message_handler
+    async def handle_generate_test_code_message(self, message: TestCodeMessage, ctx: MessageContext) -> TestCodeResult:
+        print_green(f"{self.id.type} received message. Creating now a test suite for the function.")
 
-            # Prepare input to the chat completion model.
-            prompt = "Function specification: " + message.specification + "\nFunction signature: " + message.function_signature
-            response = self._client.chat(
-                model=self._llm,
-                messages=[
-                    {'role': 'user',
-                     'content':self._system_prompt + prompt},
-                ]
-            )
+        # Prepare input to the chat completion model.
+        prompt = "Function specification: " + message.specification + "\nFunction signature: " + message.function_signature + "\nPlease generate the tests:"
+        user_message = UserMessage(content=prompt, source="user")
+        start_time = time.perf_counter()
+        response = await self._model_client.create(
+            self._system_messages + [user_message], cancellation_token=ctx.cancellation_token
+        )
 
-            prompt = "Are these tests correct? If not correct them. Do not make new tests but only inspect the one provided:\n" + response['message']['content']
-            response_corrected = self._client.chat(
-                model=self._llm,
-                messages=[
-                    {'role': 'user',
-                     'content': prompt},
-                ]
-            )
+        assert isinstance(response.content, str)
+        usage_metadata = response.usage
+        tokens = usage_metadata.prompt_tokens + usage_metadata.completion_tokens
+        end_time = time.perf_counter()
+        return TestCodeResult(response.content, end_time - start_time, tokens, ctx.cancellation_token)
 
-            dialogue(response['message']['content'] + "\n\n" + response_corrected['message']['content'], self._role)
-            return_message = await self._runtime.send_message(
-                CodeMessage(message.specification, message.function_signature, "", response_corrected['message']['content'],
-                            self.id.type), AgentId("test_executor", "default"))
-            return return_message
 
---
+++ experiments/test_designer/Designer_evaluator.py
@@ -0,0 +1,166 @@
+import logging
+import asyncio
+import sys
+import tempfile
+import time
+from autogen_core import SingleThreadedAgentRuntime, AgentId
+from autogen_core.models import ModelFamily
+from autogen_ext.code_executors.docker import DockerCommandLineCodeExecutor
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.models.ollama import OllamaChatCompletionClient
+
+from app.agents.coding_agents.TestDesigner import *
+from app.agents.coding_agents.utils.Utils import *
+from app.agents.coding_agents.utils.Code_Extractors import *
+from app.Utils import *
+from experiments.MessageTypesTest import *
+import pandas as pd
+
+
+
+async def execute_function(function: str, test:str, entry_point, executor, ctx):
+    # Installing dependencies in container
+    dependencies = "```sh\npip install numpy\n```"
+    code_block = extract_markdown_code_blocks(dependencies)
+
+    code = function + test + "\n\ncheck(" + entry_point + ")\n"
+    invocation_code = CodeBlock(code=code, language='python')
+    code_block.append(invocation_code)
+
+    start_time = time.perf_counter()
+    result = await executor.execute_code_blocks(code_block, ctx)
+    end_time = time.perf_counter()
+
+    return result, end_time - start_time
+
+
+async def main(llm, client, system_prompt):
+
+    runtime = SingleThreadedAgentRuntime()
+    await TestDesigner.register(runtime, "test_designer", lambda: TestDesigner(llm = llm, model_client=client))
+
+    work_dir = tempfile.mkdtemp()
+    executor = DockerCommandLineCodeExecutor(work_dir=work_dir)
+    await executor.start()
+
+    #Importing the dataset
+    df = pd.read_parquet("hf://datasets/evalplus/humanevalplus/data/test-00000-of-00001-5973903632b82d40.parquet")
+
+    # Creating file to store data
+    if system_prompt:
+        file_name = "coder_results/"+ llm+".parquet"
+    else:
+        file_name = "coder_results/"+ llm+"_no_prompt.parquet"
+    columns = [
+        'task_id', 'passed', 'generation_time', 'tokens',
+        'execution_time', 'tests'
+    ]
+    if os.path.exists(file_name):
+        results_df = pd.read_parquet(file_name)
+    else:
+        results_df = pd.DataFrame(columns=columns)
+
+    runtime.start()  # Start processing messages in the background.
+
+    # Iterating through each row
+    for row in df.itertuples(index=False):
+        task_id = row.task_id
+        entry_point = row.entry_point
+        prompt = row.prompt
+        test = row.test
+        canonical_solution = row.canonical_solution
+        canonical_code = prompt + canonical_solution
+
+        # Function already generated in a previous experiment
+        if task_id in results_df['task_id'].values:
+            continue
+
+        print_yellow(task_id)
+        response= await runtime.send_message(TestCodeMessage(prompt, entry_point, system_prompt), AgentId("test_designer", "default"))
+
+
+        # Generated function execution
+        test_code = extract_markdown_code_blocks(response.content)
+        test_code_string = test_code[0].code
+        result, execution_time_generated = await execute_function(canonical_code, test_code_string, entry_point, executor, response.ctx)
+        if "AssertionError" in result.output:
+            print_blue(f"\n{'-' * 130}\nExecutor:\n{result.output}\n{'-' * 130}")
+            passed = False
+        else:
+            passed = True
+
+
+        if "AssertionError" in result.output:
+            print_purple(f"\n{'-' * 130}\nExecutor:\n{result.output}\n{'-' * 130}")
+
+
+        new_data = {
+            'task_id': [str(task_id)],
+            'passed': [passed],
+            'generation_time': [response.time],
+            'tokens': [response.tokens],
+            'execution_time': [execution_time_generated],
+            'tests': [test_code_string]
+        }
+
+        new_row_df = pd.DataFrame(new_data)
+        if results_df.empty:
+            results_df = new_row_df
+        else:
+            results_df = pd.concat([results_df, new_row_df], ignore_index=True)
+
+        results_df['task_id'] = results_df['task_id'].astype('string')
+        results_df.to_parquet(file_name, engine='pyarrow')
+
+    await executor.stop()
+    await runtime.stop()  # Stop processing messages in the background.
+
+
+
+
+
+if __name__ == "__main__":
+
+    load_env_variables()
+    log_path = "../../output/designer/log"
+    set_logging_config(log_path)
+    llm = get_config_data("../config_test.yaml")
+    designer = llm['test_designer']
+    if llm['coder_prompt'] == "Yes":
+        prompt = True
+    else:
+        prompt = False
+
+    if designer == "gemini-2.5-pro" or designer == "gemini-2.0-flash":
+        model_client = OpenAIChatCompletionClient(
+            model=designer,
+            api_key=os.environ["GEMINI_API_KEY"],
+            base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+            max_retries = 10,
+            model_info={
+                "family": ModelFamily.GEMINI_2_0_FLASH,
+                "function_calling": True,
+                "json_output": True,
+                "vision": False,
+                "structured_output": True,
+            },
+        )
+    else:
+        model_client = OllamaChatCompletionClient(
+            model=designer,
+            host="160.80.97.151:11434",
+            model_info={
+                "family": ModelFamily.UNKNOWN,
+                "function_calling": True,
+                "json_output": True,
+                "vision": False,
+                "structured_output": True,
+            }
+        )
+
+    try:
+        asyncio.run(main(designer, model_client, prompt))
+    finally:
+        logging.shutdown()
+
+
