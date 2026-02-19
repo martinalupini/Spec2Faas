0aa4d868a224f389e89158773f5b1d322eaf38f2
martinalupini <martinalupini11@gmail.com>
2026-02-19 17:11:20 +0100

    graphs deployer

--- experiments/faas_deployer/deployer_results/GraphMakerDeployer.py
+++ experiments/faas_deployer/deployer_results/GraphMakerDeployer.py
@@ -1,56 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import math
-
-def make_vertical_bar_plot():
-    df_csv = pd.read_csv('../results.csv')
-
-    models = df_csv['model'].tolist()
-    metrics = df_csv.columns.drop(['model', 'avg_invocation_attempts']).tolist()
-
-    n_metrics = len(metrics)
-    cols = min(n_metrics, 3)
-    rows = math.ceil(n_metrics / cols)
-
-    fig, axes = plt.subplots(nrows=rows, ncols=cols, figsize=(6 * cols, 5 * rows))
-    axes = np.array(axes).reshape(-1)
-
-    for ax in axes.flatten()[len(metrics):]:
-        ax.set_visible(False)
-
-    fig.suptitle('', fontsize=24, weight='bold')
-
-    colors = plt.cm.viridis(np.linspace(0, 1, len(models)))
-    color_map = {model: color for model, color in zip(models, colors)}
-
-    for ax, metric in zip(axes.flatten(), metrics):
-
-        df_value = df_csv.copy()
-        models_plot = models
-        valori = df_value[metric]
-
-        plot_colors = [color_map[model] for model in models_plot]
-
-        bars = ax.bar(models_plot, valori, color=plot_colors, width=0.6)
-
-        for spine in ax.spines.values():
-            spine.set_visible(False)
-
-        ax.set_title(metric, fontsize=12, weight='bold')
-        ax.set_ylabel(metric, fontsize=10)
-        ax.tick_params(axis='x', rotation=45, labelsize=8)
-        ax.yaxis.grid(True, linestyle='--', alpha=0.6)
-
-        for bar in bars:
-            yval = bar.get_height()
-            ax.text(bar.get_x() + bar.get_width()/2.0, yval * 1.01, f'{yval:.2f}', ha='center', va='bottom', fontsize=8)
-
-    plt.subplots_adjust(left=0.05, right=0.98, top=0.92, bottom=0.15, hspace=0.5, wspace=0.25)
-
-    plt.savefig('../comparison.png', dpi=300)
-
-    plt.show()
 
 
 def make_horizontal_bar_plots(metrics, fig_lenght, title):
@@ -73,24 +23,25 @@ def make_horizontal_bar_plots(metrics, fig_lenght, title):
         bars = ax.barh(models_plot, values, color=plot_colors, height=0.6)
 
         ax.set_title(metric.replace('_', ' ').title(), fontsize=22, weight='bold')
-        ax.tick_params(axis='y', labelsize=20)
+        ax.tick_params(axis='y', labelsize=22)
+        ax.tick_params(axis='x', labelsize=18)
+
 
         ax.xaxis.grid(True, linestyle='--', alpha=0.6)
-        ax.set_xlabel(metric.replace('_', ' ').title(), fontsize=20)
+        ax.set_xlabel(metric.replace('_', ' ').title(), fontsize=22, weight='bold', labelpad=40)
 
-        for spine in ['top', 'right']:
-            ax.spines[spine].set_visible(False)
+        for spine in ax.spines.values():
+            spine.set_visible(False)
 
         for bar in bars:
             width = bar.get_width()
             ax.text(width + (max(values) * 0.01), bar.get_y() + bar.get_height() / 2,
-                    f'{width:.2f}', va='center', fontsize=16, weight='bold')
+                    f'{width:.2f}', va='center', fontsize=18, weight='bold')
 
     plt.tight_layout()
     plt.savefig(title, dpi=300)
     plt.show()
 
 
-#make_vertical_bar_plot()
-make_horizontal_bar_plots(['number_functions_correctly_deployed', 'number_functions_correctly_executed'], 20, '../comparison_horizontal_deployer_performance.png')
-make_horizontal_bar_plots(['avg_deployment_time (s)', 'avg_tokens'], 15, '../comparison_horizontal_deployer.png')
+#make_horizontal_bar_plots(['number_functions_correctly_deployed', 'number_functions_correctly_executed'], 20, '../comparison_horizontal_deployer.png')
+#make_horizontal_bar_plots(['avg_deployment_time (s)', 'avg_tokens'], 15, '../comparison_horizontal_deployer_performance.png')
